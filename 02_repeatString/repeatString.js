2c3e08e2a96785eb560adbb3384bbe208357012a
Calathea-Z <zsykes21@gmail.com>
2022-11-02 07:26:44 -0400

    First attempt at getting function to work properly. Hit a blocker. Will try again later.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
 
+const repeatString = function(word, ya) {
+for (var i = 0; i < (ya-1); i++){
+let string_new = "misc"
+string_new = (word * ya);
+console.log(string_new);
+}
+return string_new
 };
 
+repeatString('hey',3);
 // Do not edit below this line
 module.exports = repeatString;
